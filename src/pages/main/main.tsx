220981fb2a7068916e614762ab742e507b49cc49
heligie <heligiette@gmail.com>
2024-04-02 01:09:37 +0300

    feat: implement the display of data loading

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,17 +1,21 @@
 import clsx from 'clsx';
 import { useEffect } from 'react';
-import { useAppSelector, useAppDispatch } from '../../shared/lib/redux';
+import { useAppSelector, useAppDispatch, getPreviewOffersStatus } from '../../shared/lib/redux';
 import { getCurrentOffers } from '../../entities/offers';
 import { fetchPreviewOffers } from '../../entities/offers/api/thunks';
+import { APIStatus } from '../../shared/const';
 
 import Layout from '../../shared/layout';
 import Header from '../../widgets/header';
 import Filter from '../../features/filter';
 import AllOffers from '../../widgets/all-offers';
+import Loader from '../../shared/ui/loader';
 
 const Main = (): JSX.Element => {
   const dispatch = useAppDispatch();
   const currentOffers = useAppSelector(getCurrentOffers);
+  const status = useAppSelector(getPreviewOffersStatus);
+  const isLoading = status === APIStatus.Loading;
 
   useEffect(() => {
     dispatch(fetchPreviewOffers());
@@ -25,8 +29,9 @@ const Main = (): JSX.Element => {
       content={
         <main className={clsx('page__main page__main--index', { 'page__main--index-empty': !currentOffers.length })}>
           <h1 className="visually-hidden">Cities</h1>
-          <Filter />
-          <AllOffers />
+          {isLoading && <Loader />}
+          {!isLoading && <Filter />}
+          {!isLoading && <AllOffers />}
         </main>
       }
       footer={false}
--- src/shared/api/const/index.ts
+++ src/shared/api/const/index.ts
@@ -1,3 +1,3 @@
 export const API_URL = 'https://15.design.htmlacademy.pro/six-cities';
-export const AUTH_TOKEN_KEY = 'BYgwN5ayh6';
+export const AUTH_TOKEN_KEY = 'six-cities-token';
 export const REQUEST_TIMEOUT = 5000;
