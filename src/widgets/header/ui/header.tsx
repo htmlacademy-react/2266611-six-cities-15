2ad18768a5c1215e1c878003e70f7bcd264d15ac
heligie <heligiette@gmail.com>
2024-04-05 15:51:50 +0300

    feat: add a display of the user's avatar

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -57,7 +57,12 @@ const Header = ({ withToolbar = true, activeLogo = false }: HeaderProps): JSX.El
                   <>
                     <li className="header__nav-item user">
                       <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
-                        <div className="header__avatar-wrapper user__avatar-wrapper">
+                        <div className="header__avatar-wrapper user__avatar-wrapper"
+                          style={{
+                            backgroundImage: `url(${userData?.avatarUrl ?? '../img/avatar.svg'})`,
+                            borderRadius: '50%'
+                          }}
+                        >
                         </div>
                         <span className="header__user-name user__name">{userData?.email}</span>
                         <span className="header__favorite-count">3</span>
