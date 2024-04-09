5e16c6da7ce08b3bad2476bb3e456a99c89e7b88
heligie <heligiette@gmail.com>
2024-04-09 07:28:40 +0300

    refactor: remove toast props

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './app/app.tsx';
 import { Provider } from 'react-redux';
 import { store } from './app/store/app-store';
-import { ToastContainer, Zoom } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 
 const root = ReactDOM.createRoot(
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ToastContainer transition={Zoom} autoClose={2000} />
+      <ToastContainer />
       <App />
     </Provider>
   </React.StrictMode>
